521c774721dafeb4ac789dd8096f67f93f1eea76
lilawalker <lilawlker@gmail.com>
2020-06-18 17:43:53 +0100

    Update body of Twilio text message

---
+++ app/services/twilio_client.rb
@@ -0,0 +1,15 @@
+class TwilioClient
+
+  def send_text(elderly, volunteer)
+    account_sid = ENV["ACCOUNT_SID"]
+    auth_token = ENV["AUTH_TOKEN"]
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+        body: "You have received a notification to connect with #{elderly.name}. Please contact them within 48hrs on #{elderly.telephone}. To see their full profile please visit age-uk-volunteer-matching.herokuapp.com",
+        to: volunteer.telephone,
+        from: ENV["FROM"])
+
+    puts message.sid
+  end
+end
