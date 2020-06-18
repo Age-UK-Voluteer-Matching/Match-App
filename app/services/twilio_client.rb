b7624243c94de676d71cfb70cfda1ec319b5af5e
lilawalker <lilawlker@gmail.com>
2020-06-18 15:54:00 +0100

    Update Twilio with elderly and volunteer as arguments

--- app/services/twilio_client.rb
+++ app/services/twilio_client.rb
@@ -1,13 +1,13 @@
 class TwilioClient
 
-  def send_text
+  def send_text(elderly, volunteer)
     account_sid = ENV["ACCOUNT_SID"]
     auth_token = ENV["AUTH_TOKEN"]
 
     @client = Twilio::REST::Client.new account_sid, auth_token
     message = @client.messages.create(
-        body: "You have received a notification to connect with Ethel. Please contact her within 48hrs on 0778889999. To see her full profile please visit age-uk-volunteer-matching.herokuapp.com",
-        to: ENV["TO"],
+        body: "You have received a notification to connect with #{elderly.name}. Please contact her within 48hrs on #{elderly.telephone}. To see her full profile please visit age-uk-volunteer-matching.herokuapp.com",
+        to: volunteer.telephone,
         from: ENV["FROM"])
 
     puts message.sid
