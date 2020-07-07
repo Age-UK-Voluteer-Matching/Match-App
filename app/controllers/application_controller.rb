240605c52106bd25b99347f411e9fd6c7d5ca24d
naeglinghaff <kate.o.haffenden@gmail.com>
2020-07-07 12:26:28 +0100

    Added digest to application congroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
